e89be7b821d9c5abdc844ff97dc2f1627140c7f7
gaurav <34302005+gauravsinghrathore@users.noreply.github.com>
2023-03-30 11:36:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, request
+import scanpy as sc
+import pandas as pd
 
+# load adata
+adata = sc.read('path/to/adata.h5ad')
+
+# create Flask app
+app = Flask(__name__)
+
+# define homepage
+@app.route('/')
+def index
